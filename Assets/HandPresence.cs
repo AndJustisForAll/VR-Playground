cc47f8815aff6b8c2ae0ead9f4a43f89aad901f8
Angel Del Villar <amunozdv@gmail.com>
2021-01-25 21:56:25 -0600

    Testing different input methods

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -5,24 +5,40 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    private InputDevice targetDevice;
     // Start is called before the first frame update
     void Start()
     {
-        List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
 
-        foreach(var device in devices){
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightController = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightController, devices);
+
+        foreach (var device in devices)
+        {
             Debug.Log(device.name + device.characteristics);
         }
-        
-        
-        
-        
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if (primaryButtonValue)
+            Debug.Log("Pressing primary button: " + primaryButtonValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        if (triggerValue > 0.1f) Debug.Log("Pressing trigger button: " + triggerValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        if (primary2DAxisValue != Vector2.zero) Debug.Log("Primary joystick: " + primary2DAxisValue);
     }
 }
