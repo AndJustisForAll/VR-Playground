4a0b97cf84d093be5539d59a1a49717f6ccf090b
Angel Del Villar <amunozdv@gmail.com>
2021-01-25 22:31:40 -0600

    Adding controller prefabs & swapping cube controllers for Rift controllers

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -6,6 +6,9 @@ using UnityEngine.XR;
 public class HandPresence : MonoBehaviour
 {
     private InputDevice targetDevice;
+    // This will appear as a prop in Unity and could be populated by drag-drop models into list
+    private GameObject spawnedController;
+    public List<GameObject> controllerPrefabs;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,18 @@ public class HandPresence : MonoBehaviour
         if (devices.Count > 0)
         {
             targetDevice = devices[0];
+            // Device name read from InputDevice must match the Model names added in Unity
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.Log("Did not find the controller model named: " + targetDevice.name);
+                Debug.Log("Loading default controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
         }
 
 
